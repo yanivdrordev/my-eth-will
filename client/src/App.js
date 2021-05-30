b6fe1207045031f18d0c6f34d4366e2fad2cb0a7
yanivdror <yanivdrordev@gmail.com>
2021-05-31 01:11:03 +0300

    add semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import { useMetaMask } from 'metamask-react';
-import { useState, useRef } from 'react';
-import VaultFactoryContract from './contracts/VaultFactory.json';
-
-import getWeb3 from './getWeb3';
 import WillPage from './components/WillPage';
 import HomePage from './components/HomePage';
 
