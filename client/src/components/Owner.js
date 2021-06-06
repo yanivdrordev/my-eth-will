a58446e26f420fc3e82acbf87053fb508c81ca9a
yanivdror <yanivdrordev@gmail.com>
2021-06-06 23:49:19 +0300

    ui for deposit and withdraw

--- client/src/components/Owner.js
+++ client/src/components/Owner.js
@@ -1,23 +1,40 @@
 import { useEffect, useState } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Message } from 'semantic-ui-react';
 
 const Owner = ({ account, web3, contract, contractAddress }) => {
   const [contractBalance, setContractBalance] = useState(0);
+  const [depositeEth, setDepositeEth] = useState(0);
+  const [withdrawEth, setWithdrawEth] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const deposit = async () => {
-    await web3.eth.sendTransaction({
-      from: account,
-      to: contractAddress,
-      value: web3.utils.toWei('0.001', 'ether'),
-    });
+  const onDeposit = async (e) => {
+    e.preventDefault();
+    try {
+      await web3.eth.sendTransaction({
+        from: account,
+        to: contractAddress,
+        value: web3.utils.toWei(depositeEth, 'ether'),
+      });
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+
+    setDepositeEth(0);
     getContractBalance();
   };
 
-  const withdraw = async () => {
-    const res = await contract.methods
-      .ow_Withdraw(web3.utils.toWei('0.001', 'ether'))
-      .send({ from: account });
-    console.log(res);
+  const onWithdraw = async (e) => {
+    e.preventDefault();
+
+    try {
+      await contract.methods
+        .ow_Withdraw(web3.utils.toWei(withdrawEth, 'ether'))
+        .send({ from: account });
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+
+    setWithdrawEth(0);
     getContractBalance();
   };
 
@@ -33,12 +50,52 @@ const Owner = ({ account, web3, contract, contractAddress }) => {
   }, []);
 
   return (
-    <div>
-      <Button onClick={deposit}>Deposit</Button>
-      <Button onClick={withdraw}>Withdraw</Button>
-      <div>
-        balance: {web3.utils.fromWei(contractBalance.toString(), 'ether')}
-      </div>
+    <div onClick={() => setErrorMessage('')}>
+      <Grid>
+        <Grid.Column floated="left" width={5}>
+          <Header as="h1">hello contract owner</Header>
+        </Grid.Column>
+        <Grid.Column floated="right" width={5}>
+          <Header as="h2">
+            balance: {web3.utils.fromWei(contractBalance.toString(), 'ether')}
+          </Header>
+        </Grid.Column>
+      </Grid>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={5}>
+            <Form onSubmit={onDeposit}>
+              <Form.Field>
+                <input
+                  value={depositeEth}
+                  onChange={(e) => setDepositeEth(e.target.value)}
+                  label="ether"
+                  labelPosition="right"
+                />
+              </Form.Field>
+              <Button type="submit">Deposit</Button>
+            </Form>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Form onSubmit={onWithdraw} error={!!errorMessage}>
+              <Form.Field>
+                <input
+                  value={withdrawEth}
+                  onChange={(e) => setWithdrawEth(e.target.value)}
+                  label="ether"
+                  labelPosition="right"
+                />
+              </Form.Field>
+              <Button type="submit">Withdraw</Button>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          {errorMessage ? (
+            <Message error header="Oops!" content={errorMessage} />
+          ) : null}
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
