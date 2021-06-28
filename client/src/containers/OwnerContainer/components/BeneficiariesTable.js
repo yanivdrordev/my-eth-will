e9ce18f042f71bc534366da64a227bb1b0b08b38
yanivdror <yanivdrordev@gmail.com>
2021-06-29 00:43:23 +0300

    add BeneficiariesTableRow component

--- client/src/containers/OwnerContainer/components/BeneficiariesTable.js
+++ client/src/containers/OwnerContainer/components/BeneficiariesTable.js
@@ -1,7 +1,6 @@
-import { Icon, Table, Input, Grid } from 'semantic-ui-react';
-import EmailVerificationModal from '../modals/EmailVerificationModal';
-import UpdateBeneficiaryAmountModal from '../modals/UpdateBeneficiaryAmountModal';
+import { Table, Grid } from 'semantic-ui-react';
 import AddBeneficiaryModal from '../modals/AddBeneficiaryModal';
+import BeneficiariesTableRow from './BeneficiariesTableRow';
 
 const BeneficiariesTable = ({
   beneficiariesLength,
@@ -14,50 +13,17 @@ const BeneficiariesTable = ({
   onAddBeneficiary,
 }) => {
   const renderRows = () => {
+    
     return beneficiariesStructs.map((struct, index) => {
-      console.log(struct);
       return (
-        <Table.Row key={index}>
-          <Table.Cell>{struct.name}</Table.Cell>
-          <Table.Cell>{struct.email}</Table.Cell>
-          <Table.Cell>{struct.beneficiarAddress}</Table.Cell>
-          <Table.Cell textAlign="center">
-            {struct.verifiedAddress ? (
-              <Icon color="green" name="checkmark" size="large" />
-            ) : (
-              <EmailVerificationModal
-                titleWarning="IF YOU DONT USE GMAIL copy this message and sent it manually"
-                struct={struct}
-                contractAddress={contractAddress}
-              ></EmailVerificationModal>
-            )}
-          </Table.Cell>
-          <Table.Cell>
-            {struct.verifiedAddress ? (
-              /* START UPDATE BENEFIARY AMOUNT MODAL FORM */
-              <>
-                <Input disabled value={struct.amount} />
-                <UpdateBeneficiaryAmountModal
-                  title="UPDATE BENEFICIARY AMOUNT"
-                  beneficiariesStructs={beneficiariesStructs}
-                  index={index}
-                  setBeneficiariesStructs={setBeneficiariesStructs}
-                  submitBtnTitle="UPDATE BENEFICIARY AMOUNT"
-                  onUpdateBeneficiaryAmount={(e) =>
-                    onUpdateBeneficiaryAmount(e, index)
-                  }
-                ></UpdateBeneficiaryAmountModal>
-              </>
-            ) : (
-              /* END ADD BENEFICIARY MODAL FORM */
-              <>
-                <Icon name="attention" />
-                need to verified benifiary address first{' '}
-                <Input disabled value={struct.amount} />
-              </>
-            )}
-          </Table.Cell>
-        </Table.Row>
+        <BeneficiariesTableRow
+          index={index}
+          struct={struct}
+          contractAddress={contractAddress}
+          beneficiariesStructs={beneficiariesStructs}
+          setBeneficiariesStructs={setBeneficiariesStructs}
+          onUpdateBeneficiaryAmount={onUpdateBeneficiaryAmount}
+        />
       );
     });
   };
---
+++ client/src/containers/OwnerContainer/components/BeneficiariesTableRow.js
@@ -0,0 +1,58 @@
+import { Icon, Table, Input } from 'semantic-ui-react';
+import EmailVerificationModal from '../modals/EmailVerificationModal';
+import UpdateBeneficiaryAmountModal from '../modals/UpdateBeneficiaryAmountModal';
+
+const BeneficiariesTableRow = ({
+  index,
+  struct,
+  contractAddress,
+  beneficiariesStructs,
+  setBeneficiariesStructs,
+  onUpdateBeneficiaryAmount
+}) => {
+  return (
+    <Table.Row key={index}>
+      <Table.Cell>{struct.name}</Table.Cell>
+      <Table.Cell>{struct.email}</Table.Cell>
+      <Table.Cell>{struct.beneficiarAddress}</Table.Cell>
+      <Table.Cell textAlign="center">
+        {struct.verifiedAddress ? (
+          <Icon color="green" name="checkmark" size="large" />
+        ) : (
+          <EmailVerificationModal
+            titleWarning="IF YOU DONT USE GMAIL copy this message and sent it manually"
+            struct={struct}
+            contractAddress={contractAddress}
+          ></EmailVerificationModal>
+        )}
+      </Table.Cell>
+      <Table.Cell>
+        {struct.verifiedAddress ? (
+          /* START UPDATE BENEFIARY AMOUNT MODAL FORM */
+          <>
+            <Input disabled value={struct.amount} />
+            <UpdateBeneficiaryAmountModal
+              title="UPDATE BENEFICIARY AMOUNT"
+              beneficiariesStructs={beneficiariesStructs}
+              index={index}
+              setBeneficiariesStructs={setBeneficiariesStructs}
+              submitBtnTitle="UPDATE BENEFICIARY AMOUNT"
+              onUpdateBeneficiaryAmount={(e) =>
+                onUpdateBeneficiaryAmount(e, index)
+              }
+            ></UpdateBeneficiaryAmountModal>
+          </>
+        ) : (
+          /* END ADD BENEFICIARY MODAL FORM */
+          <>
+            <Icon name="attention" />
+            need to verified benifiary address first{' '}
+            <Input disabled value={struct.amount} />
+          </>
+        )}
+      </Table.Cell>
+    </Table.Row>
+  );
+};
+
+export default BeneficiariesTableRow;
